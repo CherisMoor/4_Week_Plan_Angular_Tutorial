2e9a06d88627c86a74121e9da469cc1d09320817
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-07-21 13:14:53 -0400

    Add input decorator and functions for newly added customers

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ICustomer } from 'src/app/shared/interfaces';
 
 @Component({
@@ -6,7 +6,19 @@ import { ICustomer } from 'src/app/shared/interfaces';
   templateUrl: './customers-list.component.html',
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomers: ICustomer[] = [];
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
+  filteredCustomers: any[] = [];
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
